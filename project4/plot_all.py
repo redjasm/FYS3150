99766a60bb03ec100003f4f6a2e2d0f87be47135
Joseph <josefam@ulrik.uio.no>
2024-11-13 09:12:09 +0100

    Add script to automate plot generation

---
+++ project4/plot_all.py
@@ -0,0 +1,18 @@
+import os
+
+# List of plot files to run
+plot_files = [
+    "plot_burnin.py",
+    "plot_critical_temp.py",
+    "plot_energy_dist.py",
+    "plot_phase_transition_improved.py",
+    "plot_phase_transition.py",
+    "plot_speedup.py"
+]
+
+# Run each plot file
+for file in plot_files:
+    print(f"Running {file}...")
+    os.system(f"python scripts/{file}")
+
+print("All plots generated successfully!")
